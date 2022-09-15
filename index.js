883034e8e26cce30a231a0aadc1cca4a394d5ae0
Ejaz khan <ejaztoru999@gmail.com>
2022-09-15 21:47:22 +0500

    additional things added

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 const app = express();
+import bodyParser from 'body-parser';
 const port = 3000;
 
+//*** Port listning  */
 app.listen(port, () => {
     console.log(`server is listning on port ${port}`)
 })
